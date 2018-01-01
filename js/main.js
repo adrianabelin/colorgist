b73f30092d5bb853ef4b14e800f3463d3bb03980
Adriana Belinski <adrianab@protonmail.com>
2018-01-01 17:55:40 -0600

    converted hex to rgb

--- js/main.js
+++ js/main.js
@@ -10,8 +10,13 @@ $(document).ready(function(){
 
 function processHexInput() {
   var userInput = $("#hex-input").val();
-  console.log("the user inputed: "+userInput);
-  $("#rgb-input").val(userInput); //. means to call a function 
+  console.log("the user inputed: "+(userInput[0] + userInput[1]));
+
+  var rValue = parseInt(userInput[0] + userInput[1], 16);
+  var gValue = parseInt(userInput[2] + userInput[3], 16);
+  var bValue = parseInt(userInput[4] + userInput[5], 16);
+
+  $("#rgb-input").val(rValue.toString() + gValue.toString() + bValue.toString()); //. means to call a function
 }
 
 function processRgbInput() {
