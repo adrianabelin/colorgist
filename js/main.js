b30003f892e425f2b3b72e2222e22a3b86e2f845
Adriana Belinski <adrianab@protonmail.com>
2018-01-14 16:37:46 -0600

    added hashtag and added spacing for rgb

--- js/main.js
+++ js/main.js
@@ -9,15 +9,29 @@ $(document).ready(function(){
 });
 
 function processHexInput() {
-  var userInput = $("#hex-input").val();
-  console.log("the user inputed: "+(userInput[0] + userInput[1]));
+  var userInput = $("#hex-input").val(); //obtain user input
+  //console.log("the user inputed: "+(userInput[0] + userInput[1]));
+  if (userInput[0] == "#") {
+    userInput = userInput.substr(1);
+  }
 
-  var rValue = parseInt(userInput[0] + userInput[1], 16);
+
+
+  var rValue = parseInt(userInput[0] + userInput[1], 16); //pase int converts base 16 to base 10
   var gValue = parseInt(userInput[2] + userInput[3], 16);
   var bValue = parseInt(userInput[4] + userInput[5], 16);
 
-  $("#rgb-input").val(rValue.toString() + gValue.toString() + bValue.toString()); //. means to call a function
+  var rValueString = rValue.toString(); //rValueString is the string version of rValue
+  var gValueString = gValue.toString();
+  var bValueString = bValue.toString();
+
+  var combinedString = rValueString + ", " + gValueString + ", " + bValueString;
+  //we use strings instead of ints here to prevent the program from adding the numbers like math.
+
+  $("#rgb-input").val(combinedString); //. means to call a function
+  //.val says that combinedString will be the new value for rgb-input.
 }
+
 
 function processRgbInput() {
   $("#hex-input").val("hex value");
